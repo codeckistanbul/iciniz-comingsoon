85fc80a2c71f0e871e4ce3962504f04be84b95a8
nseven07 <necipseven07@gmail.com>
2020-03-26 16:02:42 +0300

    last update

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,40 @@
 'use strict';
 
 const gulp = require('gulp');
-
-
 const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
 const gsass = require('gulp-sass');
 const cssnano = require('cssnano');
-const postcss = require('postcss');
+const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const twig = require('gulp-twig');
 const concat = require('gulp-concat');
 const connect = require('gulp-connect');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify-es').default;
-const open = require('gulp-open');
 const header = require('gulp-header');
 const imagemin = require('gulp-imagemin');
 
-const templates = './src/presentation/';
+const templates = './src/templates/';
 const dist = './dist/';
 const homePath = './src/assets/';
 const nodeModules = './node_modules/';
-const js = 'assets/js/  ';
-const css = 'assets/css/';
-const icons = 'assets/icons/';
 
+const foreach = require('gulp-foreach');
+
+const injectSvgOptions = {base: './dist/'};
+
+
+const paths = {
+    js: 'assets/js/',
+    css: 'assets/css/',
+    icons: 'assets/icons/'
+};
 
 const sourceJs = [
     nodeModules + 'jquery/dist/jquery.js',
     nodeModules + 'wowjs/dist/wow.min.js',
-    nodeModules + 'bootstrap/dist/js/bootstrap.js',
-    nodeModules + 'fullpage.js/dist/fullpage.js',
-    nodeModules + 'fullpage.js/dist/fullpage.extensions.min.js',
     homePath + 'js/main.js',
-
 ];
 
 const sourceSass = [
@@ -42,10 +42,10 @@ const sourceSass = [
 ];
 
 function compile() {
-    const twig = require('gulp-twig');
     return gulp.src('./src/templates/pages/*.html')
-        .pipe(twig({
-            base: './src/templates'
+        .pipe(foreach(function (stream, file) {
+            return stream
+                .pipe(twig({base: './src/templates'}))
         }))
         .pipe(gulp.dest(dist))
         .pipe(connect.reload());
@@ -53,9 +53,11 @@ function compile() {
 
 function scripts() {
     return gulp.src(sourceJs)
+        .pipe(sourcemaps.init())
         .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rename('scripts.min.js'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(dist + 'assets/js'))
         .pipe(connect.reload());
 }
@@ -72,7 +74,7 @@ function scriptsDev() {
 
 function connectGulp() {
     connect.server({
-        root: [dist + 'templates', 'dist'],
+        root: [dist],
         livereload: true
     });
 }
@@ -83,24 +85,32 @@ function clean() {
 
 function copyFiles() {
     return gulp
-        .src(['./src/assets/img/**/*', './src/assets/icons/**/*', './src/assets/font/**/*'], {base: './src/'})
-        //.pipe(imagemin({interlaced: true, progressive: true, optimizationLevel: 5, svgoPlugins: [{removeViewBox: true}]}))
+        .src(['./src/assets/img/**/*', './src/assets/icons/**/*', './src/assets/fonts/**/*', './src/assets/animations/**/*'
+        ], {base: './src/'})
         .pipe(gulp.dest(dist))
 }
 
 function style() {
+    const plugins = [
+        autoprefixer(),
+        cssnano({discardComments: true})
+    ];
     return gulp.src(sourceSass)
+        .pipe(sourcemaps.init({}))
         .pipe(gsass({
             includePaths: ['node_modules']
-        }))
+        }).on('error', gsass.logError))
+        .pipe(postcss(plugins))
+        .pipe(sourcemaps.write('.', {addComment: false}))
         .pipe(gulp.dest(dist + 'assets/css'))
         .pipe(connect.reload());
 }
 
+
 function myWatchTasks() {
     gulp.watch(['./src/assets/css/*.sass'], style);
     gulp.watch(['./src/assets/js/*.js'], scriptsDev);
-    gulp.watch(['./src/templates/**/*.html'], compile)
+    gulp.watch(['./src/presentation/**/*.html'], compile)
 }
 
 const dev = gulp.parallel(gulp.series(clean, style, copyFiles, scriptsDev, compile, connectGulp), myWatchTasks);
@@ -114,7 +124,3 @@ exports.build = build;
 exports.default = dev;
 exports.dev = dev;
 exports.style = style;
-
-
-
-
